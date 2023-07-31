a6574df406b4b47702eb2b755afe153fb759e1dc
zxk <280027141@qq.com>
2023-07-31 15:03:03 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  // 增强 vue-cli 的 webpack 配置项
+  configureWebpack: {
+    // 打包优化
+    externals: {
+      // import导包的包名: window全局的成员名称
+      echarts: 'echarts',
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      'element-ui': 'ELEMENT',
+      'vue-quill-editor': 'VueQuillEditor',
+      'vuex-persistedstate': 'createPersistedState'
+    }
+  }
 })
